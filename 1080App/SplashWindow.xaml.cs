b5dce98f59e75b3c02933c0aed697933b699c076
tfrasc <tfrasc@gmail.com>
2016-03-04 23:42:04 -0800

    splash screen

---
+++ 1080App/SplashWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _1080App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class SplashWindow : Window
+    {
+        public SplashWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartRect_MouseEnter(object sender, MouseEventArgs e)
+        {
+            StartRect.Opacity = 0;
+        }
+
+        private void StartRect_MouseLeave(object sender, MouseEventArgs e)
+        {
+            StartRect.Opacity = 0.5;
+        }
+
+        private void StartRect_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            StartRect.Opacity = 1;
+        }
+    }
+}
